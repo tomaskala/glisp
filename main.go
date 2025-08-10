9f6110d15956d06a0c8c79d850dddbd2389dd1a7
Tomas Kala <me@tomaskala.com>
2025-08-10 17:26:01 +0200

    Implement a script runner

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"tomaskala.com/glisp/internal/glisp"
+)
+
+const (
+	exitSuccess    = 0
+	exitIoError    = 1
+	exitParseError = 2
+	exitEvalError  = 3
+)
+
+func runRepl() int {
+	return 0
+}
+
+func runScript(path string) int {
+	source, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening %s: %v", path, err)
+		return exitIoError
+	}
+	global := glisp.NewEnv(nil)
+	parser := glisp.NewParser(path, string(source))
+	for {
+		expr, err := parser.NextExpr()
+		if errors.Is(err, glisp.EOFError{}) {
+			return exitSuccess
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			return exitParseError
+		}
+		_, err = expr.Eval(global)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			return exitEvalError
+		}
+
+	}
+}
 
 func main() {
-	fmt.Println("Hello, glisp!")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		os.Exit(runRepl())
+	}
+	os.Exit(runScript(args[0]))
 }
